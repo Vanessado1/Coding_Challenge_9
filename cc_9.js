b86ff1e7d3232e8fbb3bb306bf8e291d09cb4c87
Vanessa Do <xex.vanessa@gmail.com>
2025-02-22 11:25:22 -0500

    Task 3 - created company class

--- cc_9.js
+++ cc_9.js
@@ -39,3 +39,27 @@ const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
 console.log(mgr1.getDetails());// Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
 
 console.log(mgr1.calculateBonus()); // Expected output: 9600
+
+// Task 3: Creating a Company Class
+// created a company class 
+class Company {
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) { 
+        this.employees.push(employee); // added employee to array 
+    }
+    listEmployees() { 
+        this.employees.forEach(employee => {console.log(employee.getDetails()) // logs employee details 
+
+        });
+    }
+}
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Expected output:
+// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
